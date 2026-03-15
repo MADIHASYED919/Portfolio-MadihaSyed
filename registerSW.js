3a456c057662f1382484c57195c43ef8960797cc
MADIHASYED919 <215119895+MADIHASYED919@Users.noreply.github.com>
2026-03-16 00:22:43 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Portfolio-MadihaSyed/sw.js', { scope: '/Portfolio-MadihaSyed/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
